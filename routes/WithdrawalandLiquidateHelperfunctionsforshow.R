e54ef1b864f49f423840916865ebf25349defea9
Lin Yixiu <melodicoder@users.noreply.github.com>
2023-07-26 12:16:32 +0800

    Update WithdrawalandLiquidateHelperfunctionsforshow.R

--- routes/WithdrawalandLiquidateHelperfunctionsforshow.R
+++ routes/WithdrawalandLiquidateHelperfunctionsforshow.R
@@ -1,26 +1,27 @@
 # import database data
 source("usePackages.R")
-loadPkgs(c("sqldf","shiny","shinyalert"))
+loadPkgs(c("sqldf","shiny","shinyalert","dplyr"))
 
 
-withdrawalamount <- 1860 #for testing
-cashbalance <- 1400 #for testing
-loanData <- data.frame(loanID = c(1,2,3,4,5), loanType=c(1,2,3,2,2), loanValue = c(200, 300, 600, 300, 300), durationToMaturity = c(3,1,2,2,3)) #for testing
-# loanData <- getloanData()
-
-#loanValue is how much the loan is initially worth
-#durationToMaturity is the no. of months the loan has left before it matures
-numberofeachtypeofloan <- c(2,1) #for testing
-eachtypeofloan <- c(2,1) #for testing
-
-percentage <- 0.7
+# withdrawals <- 1860 #for testing
+# cashbalance <- 1400 #for testing
+# loanData <- data.frame(loanID = c(1,2,3,4,5), loanType=c(1,2,3,2,2), loanValue = c(200, 300, 600, 300, 300), durationToMaturity = c(3,1,2,2,3)) #for testing
+# # loanData <- getloanData()
+# 
+# #loanValue is how much the loan is initially worth
+# #durationToMaturity is the no. of months the loan has left before it matures
+# numberofeachtypeofloan <- c(1,2,0) #for testing
+# eachtypeofloan <- c(1,2,3) #for testing
+# 
+# percentage <- 0.7
 
 
 
 #### Helper Function Section
 selectLoansLiquidateModal <- function(loan.type.1.min=0, loan.type.1.max=2, 
                                       loan.type.2.min=0, loan.type.2.max=2, 
-                                      loan.type.3.min=0, loan.type.3.max=2){
+                                      loan.type.3.min=0, loan.type.3.max=2,
+                                      notenough = FALSE){
   modalDialog(
     title = "Select loans to liquidate.",
     numericInput("loantype1", "Select number of $200 Loans to liquidate", value = 0, 
@@ -29,6 +30,8 @@ selectLoansLiquidateModal <- function(loan.type.1.min=0, loan.type.1.max=2,
                  min = loan.type.2.min, max = loan.type.2.max),
     numericInput("loantype3", "Select number of $600 Loans to liquidate", value = 0,
                  min = loan.type.3.min, max = loan.type.3.max),
+    if (notenough)
+      div(tags$b("You have not selected enough loans to cover the withdrawal. Please select more loans.", style = "color: red;")),
     
     footer = tagList(
       actionButton("loanliquidatesubmission", "Confirm loans to liquidate")
@@ -44,6 +47,7 @@ updateCashBalance <- function(cashbalance, newamt){
 
 SelectLoans <- function(numberofeachtypeofloan,eachtypeofloan){
   loansselected <- data.frame(loan.type = eachtypeofloan, no.of.each.type.of.loan = numberofeachtypeofloan)
+  #need both arguments to be numerical vectors
   return(loansselected)
 }
 
@@ -52,19 +56,19 @@ LiquidateLoans <- function(cashbalance=1400, withdrawalamount=1860,
                                                loanType=c(1,2,3,2,2), 
                                                loanValue = c(200, 300, 600, 300, 300), 
                                                durationToMaturity = c(3,1,2,2,3)), 
-                           loansselected=SelectLoans(c(2,1),c(2,1)), percentage=0.7){
+                           loansselected=SelectLoans(c(1,2,0),c(1,2,3)), percentage=0.7){
   
   # Keep track of loanValue of liquidated loans
   removed_loans_value = 0
   
-  for (i in 1:nrow(loansselected)) {
+  for (i in 1:nrow(loansselected)){
     loan_type <- loansselected$loan.type[i]
     num_rows_to_remove <- loansselected$no.of.each.type.of.loan[i]
     
     # Filter the rows in loanData with loanValue equal to loan_type
     filtered_rows <- loanData[loanData$loanType == loan_type, ]
     
-    if (nrow(filtered_rows) >= num_rows_to_remove) {
+    if (num_rows_to_remove != 0 & nrow(filtered_rows) >= num_rows_to_remove) {
       # Sort the filtered rows by durationToMaturity in descending order
       sorted_rows <- filtered_rows[order(filtered_rows$durationToMaturity, decreasing = TRUE), ]
       
@@ -74,14 +78,17 @@ LiquidateLoans <- function(cashbalance=1400, withdrawalamount=1860,
       
       # Calculate the loanValue of the loans that will be removed
       removed_loans_value <- sum(rows_to_remove$loanValue) + removed_loans_value
-      print(removed_loans_value) #for debugging
+      
       
       # Remove the top num_rows_to_remove rows from loanData
       loanData <- loanData[!(loanData$loanID %in% rows_to_remove$loanID), ]
     }
-    else{
+    
+    if(nrow(filtered_rows) < num_rows_to_remove){
       stop(sprintf("You liquidated more loans of type %s than what you own",i))
     }
+    
+    print(removed_loans_value) #for debugging
   }
   
   # Update cash balance after liquidating loans and satisfying withdrawals
@@ -90,79 +97,109 @@ LiquidateLoans <- function(cashbalance=1400, withdrawalamount=1860,
   return(result_list)
 }
 
-#Note: when testing below function outside of app, comment out all the showModals and shinyAlerts.
-whenWithdrawal <- function(withdrawalamount, cashbalance, loanData, numberofeachtypeofloan, eachtypeofloan){
-  if(withdrawalamount <= cashbalance){
-    cashbalance <- updateCashBalance(cashbalance, -1*withdrawalamount)
-    print("No liquidation needed")
-  }else{
-    # showModal(...) #notification to force player to liquidate loans
-    if(percentage*sum(loanData$loanValue)+cashbalance < withdrawalamount){
-      stop("Game has ended due to inability to meet withdrawal demand")
-
-      # showModal(...) #notification to tell player the game has ended
-      shinyalert("You do not have enough loans to liquidate and cover the withdrawal. The game has ended.", type = "error")
-    }else{
-      # showModal(...) #window with drop-down selections on loans for players to select from to liquidate
-      showModal(selectLoansLiquidateModal(loan.type.1.min=0, loan.type.1.max=2, 
-                                          loan.type.2.min=0, loan.type.2.max=2, 
-                                          loan.type.3.min=0, loan.type.3.max=2)) #0s and 2s are test values
-      #TO-DO: get the min and max number of each type of loans the player has currently
-      #from database; replace the 0s and 2s with the actual values
-      
-      loansselected <- SelectLoans(numberofeachtypeofloan,eachtypeofloan) #may need to change the arguments; now assuming both are vectors of numbers
-      # print('loans selected:')
-      # print(loansselected) #for debugging
-      #TO-DO: need to find a way to retrieve the type of loan and the number of each type of loan
-      #from player's selection in drop-down lists on the loans picked
-      
-      
-      result_list <- LiquidateLoans(cashbalance, withdrawalamount, loanData, loansselected, percentage)
-      #print(percentage*result_list$removed_loans_value)
-      if(percentage*result_list$removed_loans_value+cashbalance < withdrawalamount){
-        stop("Did not meet withdrawal demand, liquidate more loans")
-
-        # showModal(...) #window to show the loans selected are not enough to cover the withdrawal
-        shinyalert("You have not selected enough loans to cover the withdrawal. Please select more loans", type = "error")
-      } else {
-        print(result_list) #for debugging
-        loanData <- result_list$resultloanData
-        cashbalance <- result_list$resultcashbalance
-      }
-    }
-  }
-  result_result_list <- list(loanData = loanData, cashbalance = cashbalance)
-  return(result_result_list)
+getMaxLoan <- function(loanData){
+  loan_type_counts <- loanData %>%group_by(loanType) %>% tally()
+  result_list <- list(one = loan_type_counts[1,2], two = loan_type_counts[2,2], three = loan_type_counts[3,2])
+  return(result_list)
 }
 
-updateCashInventory(month=3, deposits=3000, withdrawals=withdrawalamount, loanPayout=0,cashOnHand=cashbalance)      
+
+
+# updateCashInventory(month=3, deposits=3000, withdrawals=withdrawalamount, loanPayout=0,cashOnHand=cashbalance)      
 # need to update loan inventory too
-# when testing app for now, comment out above function too
 
 test <- function(){
   # test liquidate loans
   loanData <- data.frame(loanID = c(1,2,3,4,5), loanType=c(1,2,3,2,2), loanValue = c(200, 300, 600, 300, 300), durationToMaturity = c(3,1,2,2,3)) #for testing
-  numberofeachtypeofloan <- c(2,1) #for testing
-  eachtypeofloan <- c(2,1) #for testing
+  numberofeachtypeofloan <- c(1,2,0) #for testing
+  eachtypeofloan <- c(1,2,3) #for testing
   loansselected <- SelectLoans(numberofeachtypeofloan,eachtypeofloan)
-  LiquidateLoans(cashbalance=1400, withdrawalamount=1860, loanData, loansselected, percentage=0.7)
+  print(loansselected)
+  result_list <- LiquidateLoans(cashbalance=1400, withdrawalamount=1860, loanData, loansselected, percentage=0.7)
+  return(result_list)
 }
 
 #### UI
 ui <- fluidPage(
-  actionButton("start", "Start")
+  actionButton("nextmonth", "Next Month")
 )
 
 
 #### Server Section
 #the following will be in the server function after some observe event of a customer withdrawing:
 server <- function(input, output, session){
+  vals <- reactiveValues(cashOnHand = 1400, 
+                         loanData = data.frame(loanID = c(1,2,3,4,5), 
+                                               loanType=c(1,2,3,2,2), 
+                                               loanValue = c(200, 300, 600, 300, 300), 
+                                               durationToMaturity = c(3,1,2,2,3)),
+                         numberofeachtypeofloan=NULL,
+                         percentage=0.7)
 
-  observeEvent(input$start, {
-    showModal(selectLoansLiquidateModal(1,2,1,2,1,2))
+  observeEvent(input$nextmonth, {
+    print("This is the current cash balance:")
+    print(vals$cashOnHand)
+    print("This is the current loan data:")
+    print(vals$loanData)
+    
+    withdrawals <- 1860 #as test
+    #Note: when testing below chunk outside of app, comment out all the showModals and shinyAlerts.
+    if(withdrawals <= vals$cashOnHand){
+      vals$cashOnHand <- updateCashBalance(vals$cashOnHand, -1*withdrawals)
+      print("No liquidation needed")
+    }else{
+      if(vals$percentage*sum(vals$loanData$loanValue)+ vals$cashOnHand < withdrawals){
+        # print("Game has ended due to inability to meet withdrawal demand")
+        
+        #notification to tell player the game has ended
+        shinyalert("You do not have enough loans to liquidate and cover the withdrawal. The game has ended.", type = "error")
+      }else{
+        print("im here")
+        max_number_list <- getMaxLoan(vals$loanData)
+        
+        #window with drop-down selections on loans for players to select from to liquidate
+        showModal(selectLoansLiquidateModal(loan.type.1.min=0, loan.type.1.max=max_number_list$one, 
+                                            loan.type.2.min=0, loan.type.2.max=max_number_list$two, 
+                                            loan.type.3.min=0, loan.type.3.max=max_number_list$three,
+                                            notenough = FALSE))
+        
+        
+        observeEvent(input$loanliquidatesubmission, {
+          removeModal()
+          vals$numberofeachtypeofloan <- c(input$loantype1, input$loantype2, input$loantype3)
+          eachtypeofloan <- c(1,2,3)
+          req(vals$numberofeachtypeofloan)
+          loansselected <- SelectLoans(vals$numberofeachtypeofloan, eachtypeofloan) #now assuming both are vectors of numbers
+          print('loans selected:')
+          print(loansselected) #for debugging
+          vals$numberofeachtypeofloan <- NULL
+          
+          result_list <- LiquidateLoans(vals$cashOnHand, withdrawals, vals$loanData, loansselected, vals$percentage)
+          #print(percentage*result_list$removed_loans_value)
+          if(vals$percentage*result_list$removed_loans_value+vals$cashOnHand < withdrawals){
+            # print("Did not meet withdrawal demand, liquidate more loans")
+            
+            # showModal(...) #window to show the loans selected are not enough to cover the withdrawal
+            showModal(selectLoansLiquidateModal(loan.type.1.min=0, loan.type.1.max=max_number_list$one, 
+                                                loan.type.2.min=0, loan.type.2.max=max_number_list$two, 
+                                                loan.type.3.min=0, loan.type.3.max=max_number_list$three,
+                                                notenough = TRUE))
+          } else {
+            print(result_list) #for debugging
+            vals$loanData <- result_list$resultloanData
+            vals$cashOnHand <- result_list$resultcashbalance
+          }
+        })
+      }
+    }
+    
+    print("This is the current loan data:")
+    print(vals$loanData)
+    print("This is the current cash balance:")
+    print(vals$cashOnHand)
   })
-
-
+  
+  
   
 }      
 
