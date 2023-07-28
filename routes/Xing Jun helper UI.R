5be10cdba92c1d0cc521fe9438da72d1a75d00c7
AxejayL <69445998+AxejayL@users.noreply.github.com>
2023-07-28 15:57:52 +0800

    Revert "Update Xing Jun helper UI.R"
    This reverts commit 8430e6933648a50fb4f0653e9c9306b44cce0b0e.

--- routes/Xing Jun helper UI.R
+++ routes/Xing Jun helper UI.R
@@ -10,18 +10,17 @@ stateofProgressUI <- function(session){
 # Server function for the progress tracker
 serverProgressTracker <- function(input, output, loanData) {
   # Calculate the progress percentage and add it as a new column in loanData
-  silly <- loanData
-  silly$progress <- 100 * (silly$loanmaturity / 5)
+  loanData$progress <- 100 * (loanData$loanmaturity / 5)
   
   # Sort loanData by progress percentage in descending order
-  silly <- silly[order(-silly$progress), ]
+  loanData <- loanData[order(-loanData$progress), ]
   
   output$loanProgressBars <- renderUI({
     progress_bars <- list()
-    for (i in 1:nrow(silly)) {
-      loan_value <- silly$loanValue[i]
-      loan_title <- paste("Loan", silly$loanID[i])
-      progress <- silly$progress[i]
+    for (i in 1:nrow(loanData)) {
+      loan_value <- loanData$loanValue[i]
+      loan_title <- paste("Loan", loanData$loanID[i])
+      progress <- loanData$progress[i]
       
       pb <- progressBar(
         id = paste0("loan_", i),  # Unique id for each progress bar
